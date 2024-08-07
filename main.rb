893f8c7c91775285ead7c2f38353a0d6855c935c
7minutech <elijohnson1107@gmail.com>
2024-08-07 17:57:19 -0500

    Added test output to main file

---
+++ main.rb
@@ -0,0 +1,13 @@
+require_relative "lib/linked_list"
+require_relative "lib/node"
+
+list = LinkedList.new
+
+list.append("dog")
+list.append("cat")
+list.append("parrot")
+list.append("hamster")
+list.append("snake")
+list.append("turtle")
+
+puts list
